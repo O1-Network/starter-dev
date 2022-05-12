24c9784ce37e4815f919b3952b4da41090d556e3
Jim <omesadev@outlook.com>
2022-05-12 12:41:38 -0400

    Added simple styling, layout, navigation modification

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,26 @@ import './style.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
 
-      <nav class="col-full grid-container">
-        <h1 class="col-1">Starter</h1>
+      <nav className="col-full grid-container">
+        <h1 className="col-1 header">Starter</h1>
 
-        <a class="col-5 navLink" href="#">Login</a>
-        <a class="col-6 navLink" href="#">Sign Up</a>
+        <a className="col-5 navLink" href="#">Login</a>
+        <a className="col-6 navLink" href="#">Sign Up</a>
           
       </nav>
       
 
 
-      <div class="banner-container">
+      <div className="banner-container">
         
-          <div class="banner-headings">
+          <div className="banner-headings">
             <h3>First Line of Text</h3>
             <h3>Second Line of Text</h3>
           </div>
           
-          <img class="banner-image" src="https://placekitten.com/200/300"></img>
+          <img className="banner-image" src="https://placekitten.com/200/300"></img>
         
       </div>
 
