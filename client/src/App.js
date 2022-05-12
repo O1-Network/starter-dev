850ffab3f23ba1863c0a77a1669b75378a827fd5
Jim <omesadev@outlook.com>
2022-05-11 22:12:36 -0400

    NonLoggedIn Landing Page - Night One

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,46 @@
+import React from 'react';
+import MainSection from './components/MainSection';
+import './style.css';
+
 const App = () => {
   return (
     <div>
-      <h1>Starter</h1>
+
+      <nav class="col-full grid-container">
+        <h1 class="col-1">Starter</h1>
+
+        <a class="col-5 navLink" href="#">Login</a>
+        <a class="col-6 navLink" href="#">Sign Up</a>
+          
+      </nav>
+      
+
+
+      <div class="banner-container">
+        
+          <div class="banner-headings">
+            <h3>First Line of Text</h3>
+            <h3>Second Line of Text</h3>
+          </div>
+          
+          <img class="banner-image" src="https://placekitten.com/200/300"></img>
+        
+      </div>
+
+
+
+
+
+      <MainSection />
+      
     </div>
   );
 }
+
+/*class NavLinks extends React.Component {
+  createLinks() {
+    
+  }
+}*/
 
 export default App;
---
+++ client/src/components/MainSection.js
@@ -0,0 +1,45 @@
+import './MainSection.css';
+
+const MainSection = () => {
+  return (
+    <div className="main-section">
+
+      <div id="intro">
+        <h3>Business</h3>
+        <p>
+          This is a bunch of information that sounds good to a business and encourages them to work with us.
+          This section talks about the benefits of getting technology like websites, internal business tools,
+          databases, and many other options.
+        </p>
+        <a href="#">Get Started</a>
+      </div>
+
+      <div id="video">
+        <iframe
+          width="420"
+          height="315"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        />
+      </div>
+
+      <div id="image">
+        <img
+          src="http://via.placeholder.com/360x240"
+          alt="business placeholder"
+        />
+      </div>
+
+      <div id="learn-more">
+        <p>
+          If you want to see or hear more, click this link. This link is going to open up more options
+          for businesses and nonprofits to get more information about the products and services
+          available, the charity work, etc etc etc.
+        </p>
+        <a href="#">Learn More</a>
+      </div>
+
+    </div>
+  );
+};
+
+export default MainSection;
