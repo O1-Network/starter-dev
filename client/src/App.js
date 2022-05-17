a3aba12fb73f6df05b45b2be4f0337c7697a289e
BryonVerdone <bryonverdone@gmail.com>
2022-05-17 15:06:58 -0400

    added footer component to page and updated some classnames

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,39 @@
 import React from 'react';
-import MainSection from './components/MainSection';
+import MainSection from './components/mainsection/MainSection';
+import Footer from './components/footer/footer';
 import './style.css';
 
 const App = () => {
   return (
     <div>
+      <nav className='col-full grid-container'>
+        <h1 className='col-1'>Starter</h1>
 
-      <nav class="col-full grid-container">
-        <h1 class="col-1">Starter</h1>
-
-        <a class="col-5 navLink" href="#">Login</a>
-        <a class="col-6 navLink" href="#">Sign Up</a>
-          
+        <a className='col-5 navLink' href='#'>
+          Login
+        </a>
+        <a className='col-6 navLink' href='#'>
+          Sign Up
+        </a>
       </nav>
-      
 
+      <div className='banner-container'>
+        <div className='banner-headings'>
+          <h3>First Line of Text</h3>
+          <h3>Second Line of Text</h3>
+        </div>
 
-      <div class="banner-container">
-        
-          <div class="banner-headings">
-            <h3>First Line of Text</h3>
-            <h3>Second Line of Text</h3>
-          </div>
-          
-          <img class="banner-image" src="https://placekitten.com/200/300"></img>
-        
+        <img
+          className='banner-image'
+          src='https://placekitten.com/200/300'
+        ></img>
       </div>
 
-
-
-
-
       <MainSection />
-      
+      <Footer />
     </div>
   );
-}
+};
 
 /*class NavLinks extends React.Component {
   createLinks() {
---
+++ client/src/components/footer/footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './footer.css';
+export default function Footer() {
+  return (
+    <footer>
+      <div className='logo-container'>
+        <h2>Logo</h2>
+      </div>
+      <section className='footer-links'>
+        <article>
+          <h3>Links</h3>
+          <ul>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+          </ul>
+        </article>
+        <article>
+          <h3>Links</h3>
+          <ul>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+          </ul>
+        </article>
+        <article>
+          <h3>Links</h3>
+          <ul>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+          </ul>
+        </article>
+      </section>
+    </footer>
+  );
+}
