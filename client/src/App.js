6edc2abe234efd33f4303dcbb0eee9048f86090c
BryonVerdone <bryonverdone@gmail.com>
2022-05-19 11:44:47 -0400

    created and imported footer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,43 +4,40 @@ import Navigation from './components/Navigation';
 import Intro from './components/Intro';
 import Toggle from './components/Toggle';
 import MainSection from './components/MainSection';
-
+import Footer from './components/Footer';
 import BusinessData from './data/business.json';
 import DeveloperData from './data/developer.json';
 
-console.log(BusinessData)
+console.log(BusinessData);
 
 const App = () => {
+  const [sectionType, setSectionType] = useState(DeveloperData);
 
-  const[sectionType, setSectionType] = useState(DeveloperData);
-
-  console.log(sectionType)
+  console.log(sectionType);
 
   const handleClick = (e) => {
     console.log(e);
     console.log(e.target);
 
-
-    if(e.target.id === "developer-button"){
+    if (e.target.id === 'developer-button') {
       setSectionType(DeveloperData);
     } else {
       setSectionType(BusinessData);
     }
-  } 
+  };
 
   return (
-    <div className="app-container">
-
+    <div className='app-container'>
       <Navigation />
-      
-      <Intro/>
+
+      <Intro />
 
       <Toggle handleClick={handleClick} />
 
       <MainSection sectionType={sectionType} />
-      
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Footer.css';
+export default function Footer() {
+  return (
+    <footer>
+      <div className='logo-container'>
+        <h2>Logo</h2>
+      </div>
+      <section className='footer-links'>
+        <article>
+          <h3>Links</h3>
+          <ul>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+          </ul>
+        </article>
+        <article>
+          <h3>Links</h3>
+          <ul>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+          </ul>
+        </article>
+        <article>
+          <h3>Links</h3>
+          <ul>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+            <a href='#'>Link</a>
+          </ul>
+        </article>
+      </section>
+    </footer>
+  );
+}
