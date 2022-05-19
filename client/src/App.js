da80e797679c8386cb6c4f8972c79449c0a0e68a
Jim <omesadev@outlook.com>
2022-05-19 10:13:44 -0400

    Created JSON files for busienss and developer data

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,20 @@
 import React, { useState } from 'react';
-
+import './App.css';
 import Navigation from './components/Navigation';
 import Intro from './components/Intro';
 import Toggle from './components/Toggle';
 import MainSection from './components/MainSection';
 
+import BusinessData from './data/business.json';
+import DeveloperData from './data/developer.json';
+
+console.log(BusinessData)
+
 const App = () => {
 
-  const businessDemo = {
-    type: "Business demo type",
-    text: "This is a bunch of information that sounds good to a business and encourages them to work with us. This section talks about the benefits of getting technology like websites, internal business tools, databases, and many other options.",
-    details: "This is the business info",
-    video: "https://www.youtube.com/embed/tgbNymZ7vqY",
-    img: "http://via.placeholder.com/360x240",
-    imgAlt: "business placeholder",
-    learn: "If you want to see or hear more, click this link. This link is going to open up more options for businesses and nonprofits to get more information about the products and services available, the charity work, etc etc etc."
-  }
+  const[sectionType, setSectionType] = useState(DeveloperData);
 
-  const developerDemo = {
-    type: "Developer demo type",
-    text: "This is a bunch of information about the benefits of using this website for a developer. Developers should use this site because they will be benefited in all of these ways.",
-    details: "This is the developer info",
-    video: "https://www.youtube.com/embed/tgbNymZ7vqY",
-    img: "http://via.placeholder.com/360x240",
-    imgAlt: "developer placeholder",
-    learn: "If you want to see or hear more, click this link. This link is going to open up more options for developers to get more information about the products and services available, etc etc etc."
-  } 
-
-  const[sectionType, setSectionType] = useState(developerDemo);
+  console.log(sectionType)
 
   const handleClick = (e) => {
     console.log(e);
@@ -35,9 +22,9 @@ const App = () => {
 
 
     if(e.target.id === "developer-button"){
-      setSectionType(developerDemo);
+      setSectionType(DeveloperData);
     } else {
-      setSectionType(businessDemo);
+      setSectionType(BusinessData);
     }
   } 
 
