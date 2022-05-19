2e3f1a4483bc3a2adde633796c002073e519396b
jchav5150 <johnericchavez@gmailcom>
2022-05-18 19:52:09 -0600

    Updated bug for reactDOM.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
