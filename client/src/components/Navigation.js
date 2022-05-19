8b68c8eeabe49db9d390cd0e37dea976cfc6956b
AngelaDo45 <angela.do45@gmail.com>
2022-05-19 01:16:40 -0700

    Adds styling to login and signup buttons

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,8 +3,8 @@ const Navigation = () => {
         <nav className="col-full grid-container">
             <h1 className="col-1">Starter</h1>
 
-            <a className="col-5 navLink" href="www.github.com">Login</a>
-            <a className="col-6 navLink" href="www.github.com">Sign Up</a>     
+            <a className="col-5-navLink" href="www.github.com">Login</a>
+            <a className="col-6-navLink" href="www.github.com">Sign Up</a>     
         </nav>
     )
 }
