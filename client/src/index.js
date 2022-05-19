497b1f69269dbf474fe3a2b4f3529d63762831e8
BryonVerdone <bryonverdone@gmail.com>
2022-05-19 14:24:44 -0400

    replaced ReactDom with React StrictMode

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
