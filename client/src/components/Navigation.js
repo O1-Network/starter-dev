1daca1db16dee30700bb336bfbc093889a570ddf
questerson <yeadoneric@gmail.com>
2022-05-22 01:45:08 -0400

    Basic site layout adjustments Scaling and such

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,14 +1,15 @@
 import "./Navigation.css"
 const Navigation = () => {
     return (
-        <nav className="col-full grid-container">
-            
-            <h1 className="col-1">Starter</h1>
+         <nav className="col-full grid-container">
+            <div className="header clearfix">
+            <h1 className="col-1">STARTER</h1>
             <div className="btns-container">
             <a className="col-5 navLink-buttons Login" href="http://www.github.com"> Login</a>
             <a className="col-6 navLink-buttons SignUp" href="http://www.github.com">Sign Up</a>   
             </div>
-        </nav>
+            </div>
+         </nav>
     )
 }
 
