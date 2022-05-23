d6e8343dccb25449770a097cd2a1f24f8f5f19e4
questerson <yeadoneric@gmail.com>
2022-05-22 21:59:39 -0400

    issue 11 Added a temp logo

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,7 +3,7 @@ const Navigation = () => {
     return (
          <nav className="col-full grid-container">
             <div className="header clearfix">
-            <h1 className="col-1">STARTER</h1>
+            <h1 className="col-1"><img id="logo" alt='STARTER Logo' src={require('../images/logo2.png')}></img></h1>
             <div className="btns-container">
             <a className="col-5 navLink-buttons Login" href="http://www.github.com"> Login</a>
             <a className="col-6 navLink-buttons SignUp" href="http://www.github.com">Sign Up</a>   
