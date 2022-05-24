d24f31df1f214614eef9e111c52b0059b84cd638
SitaRamSai <sitaramsai.koya20.com>
2022-05-23 23:16:45 -0500

    Updated navigation css and Intro css

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -1,11 +1,11 @@
 import './Intro.css';
 const Intro = () => {
     return (
-        <div id="intro">
-          <div id="intro-statement">
+        <div className="intro">
+          <div className="intro-statement">
                 <h2>I will not look for you, I will not pursue you. But if you don't, I will look for you, I will find you, and I will kill you.</h2>
           </div>
-          <img id="intro-image" alt='Intro' src={require('../images/team_photo.jpg')}></img>
+          <img className="intro-image" alt='Intro' src={require('../images/team_photo.jpg')}></img>
         </div>
     );
 }
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,14 +4,16 @@ const Navigation = () => {
     return (
         <nav>
             <h1 className="brand-name">Starter</h1>
+            <div className="nav-menu">
+                <form action="www.github.com" id="login">
+                    <input type="submit" value="Login" />
+                </form>
 
-            <form action="www.github.com" id="login">
-                <input type="submit" value="Login" />
-            </form>
-
-            <form action="www.github.com" id="signup">
-                <input type="submit" value="Sign up" />
-            </form>     
+                <form action="www.github.com" id="signup">
+                    <input type="submit" value="Sign up" />
+                </form>   
+            </div>
+              
         </nav>
     )
 }
