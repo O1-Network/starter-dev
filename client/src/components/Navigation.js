1396970c1b1c2d51d4ab0ec9c45ad00913fd6f59
questerson <yeadoneric@gmail.com>
2022-05-25 19:14:14 -0400

    redesign issue11

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = () => {
             <h1 className="col-1"><img id="logo" alt='STARTER Logo' src={require('../images/logo2.png')}></img></h1>
             <div className="btns-container">
             <a className="col-5 navLink-buttons Login" href="http://www.github.com"> Login</a>
-            <a className="col-6 navLink-buttons SignUp" href="http://www.github.com">Sign Up</a>   
+            {/* <a className="col-6 navLink-buttons SignUp" href="http://www.github.com">Sign Up</a>    */}
             </div>
             </div>
          </nav>
