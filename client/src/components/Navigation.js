affd6452d19cbfcfbffde258ee88319614117d98
Jim <omesadev@outlook.com>
2022-05-28 21:33:37 -0400

    Completed Home Page

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,15 +4,8 @@ const Navigation = () => {
     return (
         <nav>
             <h1 className="brand-name">Starter</h1>
-            <div className="nav-menu">
-                <form action="www.github.com" id="login">
-                    <input type="submit" value="Login" />
-                </form>
-
-                <form action="www.github.com" id="signup">
-                    <input type="submit" value="Sign up" />
-                </form>   
-            </div>
+            <button className="nav-button">Login</button>
+            <button className="nav-button">Signup</button>
         </nav>
     )
 }
