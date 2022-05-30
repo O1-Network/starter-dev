94f760c8cd40243b0efda65b782151e39d999f28
Jim <omesadev@outlook.com>
2022-05-29 23:32:18 -0400

    Added NAICS paths, Visual Intro planning, and server, database and login planning

---
+++ client/src/components/MainSection.jsx
@@ -0,0 +1,16 @@
+import './MainSection.css';
+
+const MainSection = (props) => {
+  return (
+    <>
+      <div className="main_section-container">
+        <h1 className="main_section-question">We need Python Developers or people who work with data.</h1>
+        
+        
+
+      </div>
+    </>
+  );
+};
+
+export default MainSection;
