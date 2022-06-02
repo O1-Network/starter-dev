46a2d3231b199f10fb7e4101730eecd1cafab6bf
Jim <omesadev@outlook.com>
2022-06-01 22:48:22 -0400

    Erics Signup and Intro changes

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -4,7 +4,9 @@ const Signup = () => {
   return (
     <div className="container-signup">
       <div className="form-box">
-      <form className="form-signup">
+      <h1 className="form-sign-close">X</h1>
+      <form className="form-signup-h1">
+        
         <h2 className='signup-title'> Starter Business Sign-up </h2>
         <ul className='signup-list'>
           <li>
@@ -26,16 +28,28 @@ const Signup = () => {
             <input type="password" id="re-password" required />
           </li>
         </ul>
-        <div className='rules-errors'> Email, Username or Password rules and errors. . </div>
-        <button className='signup-btn'>Sign Up</button>
+        <div className='rules-errors'> <span> Email, Username or Password rules and errors. . . </span></div>
+          <div className="Signup-btnHolder">
+            <button className='signup-btn'>Sign Up</button>
         
-        <div className='back-arrow'> <img src='#' alt='Back Arrow'></img>back </div>
-
+            {/* <div className='back-arrow'> <img src='#' alt='Back Arrow'></img>back </div> */}
+          </div>
       </form>
       </div>
     </div>
-    
+                              
   )
 }
+
+      
+// document.querySelector('.container-signup').addEventListener('click', open);
+// function open(){
+// document.querySelector('.container-signup').style.display ='flex'
+// };
+// document.querySelector('.form-sign-close').addEventListener('click', close);
+// function close(){
+// document.querySelector('.container-signup').style.display ='flex'
+// };
+
 
 export default Signup;
