d4cdc0b6f96b05656b578322fca9c27d2f5dac90
Jim <omesadev@outlook.com>
2022-06-10 22:28:07 -0400

    Added styling to Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,34 +3,69 @@ import styled from 'styled-components';
 
 const HomeContainer = styled.div`
   flex-grow: 1;
-  background-color: #F2FCFF;
+  background-color: #f2fcff;
   margin: auto 20%;
   height: inherit;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  border: 1px solid black;
 `;
 
 const UserLink = styled(Link)`
   border: 1px solid black;
-  border-radius: 10px;
-  padding: 10px 20px;
   text-decoration: none;
-  color: black;
+
+  font-size: 50px;
   vertical-align: center;
-  height: 20px;
-  width: auto;
+  border: none;
+  height: 200px;
+  width: 200px;
+  padding: 100px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  font-family: 'Julius Sans One';
+  color: white;
 `;
 
+const RedUserLink = styled(UserLink)`
+  background: #812320;
+`;
+
+const BlueUserLink = styled(UserLink)`
+  background: #20428c;
+`;
+
+const WelcomeContainer = styled.div`
+ 
+  font-family: 'Julius Sans One';
+  text-align: center;
+  color: black;
+
+`;
+const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-around;
+`;
 const Home = () => {
-    return (
-        <HomeContainer>
-          <UserLink to="/business">Business</UserLink>
-          <UserLink to="/developer">Developer</UserLink>
-        </HomeContainer>
-    );
-  }
-  
-  export default Home;
+  return (
+    <HomeContainer>
+      <WelcomeContainer>
+        <h1>Welcome!</h1>
+        <h2>What best describes you?</h2>
+      </WelcomeContainer>
+
+      <LinksContainer>
+        <BlueUserLink to="/developer">Developer</BlueUserLink>
+        <RedUserLink to="/business">Business</RedUserLink>
+      </LinksContainer>
+    </HomeContainer>
+  );
+};
+
+export default Home;
