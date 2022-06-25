bc0ab60ed577ffa3d77c89330e4ab3f38b8713dc
omaranBazna <omran4d@gmail.com>
2022-06-24 22:25:35 -0400

    add some styling and effects to the contact form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,35 +4,114 @@ import {
   TextInput,
   SelectInput,
   TextAreaInput,
-  SubmitButton
+  SubmitButton,
+  NameField,
+  FieldArea,
+  ContactCard
 } from './ContactStyles';
+import React, {useState}  from 'react'
+/*
+*idea: make the field horizontal          saxx // done
+*idea: put first and last name at the same line // done
+*idea: add introduction animation      saxx // done 
+*idea :prevent the resizable textarea // done
+ 
 
+
+omaran 
+*idea :add some missing fields like email
+*idea :remove additonal unneccerly fields   countries
+*idea :font stlying
+*idea :padding and margins
+*idea :conver the form to React --implement onChange track value 
+
+
+
+angie-La
+*idea :add side card                  
+*idea: add subject 
+*idea :focus effect          
+
+
+them:
+  background-color:  steelblue;
+  form-background: rgba(0, 0, 0, 0.4);
+  text-color:white;
+  font:arial
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300&display=swap" rel="stylesheet">
+
+
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Spectral:wght@200&display=swap" rel="stylesheet">
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Spectral:wght@200&display=swap');
+</style>
+
+
+*/
 const Contact = () => {
+  const [firstname,setFirstName] = useState("");
+  const [lastname,setLastName]  = useState("");
+  const [subject, setSubject]=useState("");
+  const [email,setEmail] = useState("");
+      
+
+  
   return (
       <ContactContainer>
-        <h1>We would love to hear from you. Please get in touch and reach one of our application specialists. </h1>
+
+
+      <ContactCard>
+          Hello world
+      </ContactCard>
+
+      
+
           <FormContainer>
+     
             <form action="action_page.php" >
-          
-              <label htmlFor="fname">First Name</label>
-              <TextInput type="text" id="fname" name="firstname" placeholder="Your name.." />
-          
-              <label htmlFor="lname">Last Name</label>
-              <TextInput type="text" id="lname" name="lastname" placeholder="Your last name.." />
-          
-              <label htmlFor="country">Country</label>
-              <SelectInput id="country" name="country">
+              <NameField>
+                <FieldArea>
+                <TextInput type="text" id="fname" name="firstname" onChange={()=>{setFirstName(this.target.value)}}/>
+                  <span> First Name </span>
+                </FieldArea>
+                <FieldArea>
+                  <TextInput type="text" id="lname" name="lastname" onChange={()=>{setLastName(this.target.value)}} />
+                  <span>Last Name</span>
+                  </FieldArea>
+                 </NameField>
+                 <label htmlFor="country">Country</label>
+                <SelectInput id="country" name="country">
                 <option value="australia">Australia</option>
                 <option value="canada">Canada</option>
                 <option value="usa">USA</option>
               </SelectInput>
+
+              <FieldArea>
+                <TextInput type="email" id="email" name="email" onChange={()=>{setEmail(this.target.value)}}></TextInput>
+                <span>email</span>
+              </FieldArea>
+
+              <FieldArea>
+                <TextInput type="text" id="title" name="title" ></TextInput>
+                <span>Title</span>
+              </FieldArea>
           
               <label htmlFor="subject">Subject</label>
-              <TextAreaInput id="subject" name="subject" placeholder="Write something.."></TextAreaInput>
+              <TextAreaInput id="subject" name="subject" rows={5} placeholder="Write something.." onChange={()=>{setSubject(this.target.value)}}></TextAreaInput>
           
               <SubmitButton type="submit" value="Submit" />
           
             </form>
+            
+            
           </FormContainer> 
       </ContactContainer>
     );
--- client/src/components/ContactStyles.js
+++ client/src/components/ContactStyles.js
@@ -1,25 +1,48 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components'
+
+const fadeAnimation = keyframes`
+ 0% { opacity : 0; transform: translateY(20px); }
+ 100% { opacity : 1; transform: translateY(0px);}
+`
 
 export const ContactContainer = styled.div`
   margin: 0 20%;
   width: 40%;
+  display:flex;
+  flex-direction:row;
+  justify-content:space-between;
+  align-items:center;
+  height:600px;
 `;
+export const ContactCard = styled.div`
+height:600px;
 
+width:100px;
+background:red;
+`;
 export const FormContainer = styled.div`
   border-radius: 5px;
   background-color: #f2f2f2;
   padding: 20px;
+  animation-name: ${fadeAnimation};
+  animation-duration: 1s;
+  animation-iteration-count: easy-in;
 `;
 
 export const TextInput = styled.input`
   width: 100%;
   padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  border: none;
+  outline: none;
+  border-bottom: 3px solid #ccc;
   box-sizing: border-box;
   margin-top: 6px;
   margin-bottom: 16px;
   resize: vertical;
+  &:focus {
+    border-bottom-color: #BA1200;
+  }
+
 `;
 
 export const SelectInput = styled.select`
@@ -41,8 +64,13 @@ export const TextAreaInput = styled.textarea`
   box-sizing: border-box;
   margin-top: 6px;
   margin-bottom: 16px;
-  resize: vertical;
-`;
+  resize: none;
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 2px red;
+  }
+
+`
 
 export const SubmitButton = styled.input`
   background-color: #04AA6D;
@@ -55,4 +83,51 @@ export const SubmitButton = styled.input`
   &:hover {
     background-color: #45a049;
   }
-`;
+`
+
+
+export const FieldArea = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction : column;
+  align-items: flex-start;
+  position: relative;
+  
+  &> input:focus ~ span, 
+  &> input:placeholder-shown ~ span {
+    font-size: 14px;
+    position: absolute;
+    top: -5px;
+    left: 0px;
+    transform: translateY(-50%);
+    color: black;
+    font-style: bold;
+  }
+
+  &> input ~ span {
+    font-size: 12px;
+    position: absolute;
+    top: 40%;
+    left: 10px;
+    transform: translateY(-50%);
+    color: gray;
+    transition: all 0.5s;
+  }
+
+ 
+`
+
+export const NameField = styled.div `
+  flex-shrink: 1;
+  width : 100%;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  justify-content: space-between;
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 2px red;
+  }
+`
+
+
